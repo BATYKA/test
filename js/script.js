451085d51a7fba1b053b6c06b814d0cbcf374fd9
Slava <captainbatyka@gmail.com>
2018-12-26 14:27:09 +0300

    Прописал всплывающее окно в JS

--- js/script.js
+++ js/script.js
@@ -1,5 +1,11 @@
-<script>
-	var opnbtn
-	
-	element.classList.remove();
-</script>
+var opn = document.getElementById('popUpButton'),
+	cls = document.getElementById('buttonClose'),
+	wndw = document.getElementById('popUp');
+
+opn.addEventListener('click', function() {
+	wndw.classList.remove('visually-hidden');
+});
+
+cls.addEventListener('click', function() {
+	wndw.classList.add('visually-hidden');
+});

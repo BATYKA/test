ac4932b52aefa1af3a2956dd6812072334a44336
Slava <captainbatyka@gmail.com>
2019-01-04 16:33:44 +0300

    Сделал кнопки для переключения слайдера

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
 var opn = document.getElementById('popUpButton'),
 	cls = document.getElementById('buttonClose'),
-	wndw = document.getElementById('popUp');
+	wndw = document.getElementById('popUp'),
+	btn1 = document.getElementById('prB1'),
+	btn2 = document.getElementById('prB2'),
+	btn3 = document.getElementById('prB3');
 
 opn.addEventListener('click', function() {
 	wndw.classList.remove('visually-hidden');
@@ -8,4 +11,34 @@ opn.addEventListener('click', function() {
 
 cls.addEventListener('click', function() {
 	wndw.classList.add('visually-hidden');
+});
+
+btn1.addEventListener('click', function() {
+	btn1.classList.add('presentation-button1-alt');
+	if (btn2.classList.contains('presentation-button2-alt')) {
+		btn2.classList.remove('presentation-button2-alt');
+	}
+	if (btn3.classList.contains('presentation-button3-alt')) {
+		btn3.classList.remove('presentation-button3-alt');
+	}
+});
+
+btn2.addEventListener('click', function() {
+	btn2.classList.toggle('presentation-button2-alt');
+	if (btn1.classList.contains('presentation-button1-alt')) {
+		btn1.classList.remove('presentation-button1-alt');
+	}
+	if (btn3.classList.contains('presentation-button3-alt')) {
+		btn3.classList.remove('presentation-button3-alt');
+	}
+});
+
+btn3.addEventListener('click', function() {
+	btn3.classList.toggle('presentation-button3-alt');
+	if (btn1.classList.contains('presentation-button1-alt')) {
+		btn1.classList.remove('presentation-button1-alt');
+	}
+	if (btn2.classList.contains('presentation-button2-alt')) {
+		btn2.classList.remove('presentation-button2-alt');
+	}
 });
